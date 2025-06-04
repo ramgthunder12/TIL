1d239b10bf06598ee7b044e8eaa6d9b71465a8c6
Ubuntu ramg <ramgthunder12@gmail.com>
2025-06-04 17:31:28 +0900

    :test_tube:TIL : java - 변수와 타입(이것이자바다35p)

---
+++ java/This_is_Java/src/ch02/IntegerLiteralExample.java
@@ -0,0 +1,12 @@
+public class IntegerLiteralExample{
+	public static void main(String[] args) {
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 0xcf02;
+		int var4 = 365;
+		System.out.println("var1 : " + var1);
+		System.out.println("var2 : " + var2);
+		System.out.println("var3 : " + var3);
+		System.out.println("var4 : " + var4);
+	}
+}
