a51679d8f664ea84ef579a34c5004101100d5283
ramgthunder12 <122340555+ramgthunder12@users.noreply.github.com>
2025-06-28 20:26:25 +0900

    :bug: fix: 배열 length 메서드 호출 오류, 소수점 출력 포맷 수정

--- java/dev_skill_interview_note/NumbersRate.java
+++ java/dev_skill_interview_note/NumbersRate.java
@@ -13,6 +13,7 @@ public class NumbersRate{
     int positiveCount = 0;
     int zeroCount = 0;
     int negativeCount = 0;
+    
     for(int item : typeTemp) {
       if(item == 1) {
         positiveCount++;
@@ -26,16 +27,16 @@ public class NumbersRate{
     float zeroRate = (float) zeroCount / typeTemp.length;
     float negativeRate = (float) negativeCount / typeTemp.length;
 
-    System.out.printf("positive rate : %.6\n", positiveRate);
-    System.out.printf("zero rate : %.6\n", zeroRate);
-    System.out.printf("negative rate : %.6\n", negativeRate);
+    System.out.printf("positive rate : %.6f\n", positiveRate);
+    System.out.printf("zero rate : %.6f\n", zeroRate);
+    System.out.printf("negative rate : %.6f\n", negativeRate);
   }
   
   private void numberTypeRate(int[] numbers) {
     int[] typeTemp = new int[numbers.length];
 
     //유형 판단
-    for(int i = 0; i < numbers.length(); i++) {
+    for(int i = 0; i < numbers.length; i++) {
       typeTemp[i] = numberType(numbers[i]);
     }
     //비율 개산
