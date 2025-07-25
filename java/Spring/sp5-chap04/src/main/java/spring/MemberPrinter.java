3e3ce2d795c4ce350c290891d3697be3c5e76c4c
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-07-25 23:23:51 +0900

    sparkles: feat: practice optinal dependency injection with @Autowired, Optional and @Nullable in Spring

--- java/Spring/sp5-chap04/src/main/java/spring/MemberPrinter.java
+++ java/Spring/sp5-chap04/src/main/java/spring/MemberPrinter.java
@@ -3,6 +3,7 @@ package spring;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 
 public class MemberPrinter {
 //	public void print(Member member) {
@@ -16,13 +17,27 @@ public class MemberPrinter {
 			System.out.printf("회원 정보 : 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %tF\n"
 					, member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
 		} else {
-			System.out.printf("회원 정보 : 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %tF\n"
+			System.out.printf("회원 정보 : 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %s\n"
 					, member.getId(), member.getEmail(), member.getName(), dateTimeFormatter.format(member.getRegisterDateTime()));
 		}
 	}
 	
+//	@Autowired(required = false)
+//	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+//		this.dateTimeFormatter = dateTimeFormatter;
+//	}
+	
+//	@Autowired
+//	public void setDateFormatter(Optional<DateTimeFormatter> formatterOpt) {
+//		if(formatterOpt.isPresent()) {
+//			this.dateTimeFormatter = formatterOpt.get();
+//		} else {
+//			this.dateTimeFormatter = null;
+//		}
+//	}
+	
 	@Autowired
-	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+	public void setDateFormatter(@Nullable DateTimeFormatter dateTimeFormatter) {
 		this.dateTimeFormatter = dateTimeFormatter;
 	}
 }
