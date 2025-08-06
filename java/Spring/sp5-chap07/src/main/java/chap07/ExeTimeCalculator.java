22420f4e50a693400043ee9c04786f6f821be2a6
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-08-06 18:18:08 +0900

    :sparkles: feat: create sp5-chap07 project and implement proxy for measuring factorial execution time

---
+++ java/Spring/sp5-chap07/src/main/java/chap07/ExeTimeCalculator.java
@@ -0,0 +1,19 @@
+package chap07;
+
+public class ExeTimeCalculator implements Calculator{
+	private Calculator delegate;
+	
+	public ExeTimeCalculator(Calculator delegate) {
+		this.delegate = delegate;
+	}
+	
+	@Override
+	public long factorial(long num) {
+		long start = System.nanoTime();
+		long result = delegate.factorial(num);
+		long end = System.nanoTime();
+		System.out.printf("ImpeCalculator.factorial(%d) 실행 시간 = %d\n", num, (end-start));
+		
+		return result;
+	}
+}
