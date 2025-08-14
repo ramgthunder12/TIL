1a5bcdca61c40e758df979f54f09d3577fd76716
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-08-14 21:06:12 +0900

    :construction: chore: add exploratory comments while studying unfamiliar code

--- java/Spring/sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
+++ java/Spring/sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
@@ -8,14 +8,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect
+@Aspect //공통기능
 public class ExeTimeAspect {
-	@Pointcut("execution(public * chap07 ..*(..))")
+	@Pointcut("execution(public * chap07 ..*(..))") //공통기능을 적용할 대상
 	private void publicTarget() {
 	}
 	
-	@Around("publicTarget()")
-	public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
+	@Around("publicTarget()") //around  advice, 공통기능 적용, publicTarget() : chap07에 위치한 빈 객체의 public 메소드에 @Around가 붙은 measure()에 적용
+	public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {//ProceedingJoinPint를 프록시 대상 객체 메소드 호출할 때 사용됨
 		long start = System.nanoTime();
 		try {
 			Object result = joinPoint.proceed();
