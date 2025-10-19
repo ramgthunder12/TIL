9b9ced40ab6387545e14c71b2e3fb16b13c2ad08
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-10-19 20:00:49 +0900

    :seedling: study: Add decompressString method for string compression practice

--- java/algorithms/CompressString.java
+++ java/algorithms/CompressString.java
@@ -1,9 +1,11 @@
 public class CompressString {
     public static void main(String[] args) {
-        String str = "aaabbbcccdde";
+        String str = "aabccc";
         String compressedStr = compressString(str);
+        String decompressedStr = decompressString(compressedStr);
         System.out.println("압축 전 문자열: " + str);
         System.out.println("압축 후 문자열: " + compressedStr);
+        System.out.println("압축 해제 후 문자열: " + decompressedStr);
     }
 
     static String compressString(String str) {
@@ -25,6 +27,33 @@ public class CompressString {
                 }
             }
 
+        }
+        return answer;
+    }
+
+    static String decompressString(String str) {
+        String strPlusSpase = str + " ";
+        String answer = "";
+
+        for(int i = 1; i < strPlusSpase.length(); i++) {
+            char firstCharater = strPlusSpase.charAt(i - 1);
+            char secendCharater = strPlusSpase.charAt(i);
+            //두번째 글짜가 숫자 이면, 숫자 만큼 반복문 돌리기
+            if(Character.isDigit(secendCharater)) {
+                String secendCharaterStr = String.valueOf(secendCharater);
+                int secendCharaterInt = Integer.parseInt(secendCharaterStr);
+                String firstString = "";
+
+                for(int j = 0; j < secendCharaterInt; j++) {
+                    firstString = firstString + firstCharater;
+                }
+                answer = answer + firstString;
+            } else {
+                if(Character.isDigit(firstCharater)) {
+                    continue;
+                }
+                answer = answer + firstCharater;
+            }
         }
         return answer;
     }
