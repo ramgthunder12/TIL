f3186c8c3e628f27e9c2666dd2a247fd49bb843b
ChaeDam Bang <122340555+ramgthunder12@users.noreply.github.com>
2025-11-02 22:50:58 +0900

    :brain: study: Add comment explaining proxy type compatibility

--- java/Spring/sp5-chap07/src/main/java/config/AppCtx.java
+++ java/Spring/sp5-chap07/src/main/java/config/AppCtx.java
@@ -9,7 +9,7 @@ import chap07.Calculator;
 import chap07.RecCalculator;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy(proxyTargetClass = true) //자바 코드, "calculator" 프록시의 실제 타입은 RecCalculator를 상속받았으므로 RecCalculator로 타입 변환 가능
 //@Enable ... : "..." 관련 기능을 적용하는데 필요한 다양한 스프링 설정 대신 처리 
 public class AppCtx {
 	@Bean
