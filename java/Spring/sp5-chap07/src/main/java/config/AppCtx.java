ec5f739506f3081f7726aceb4519eda8c6a8e555
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-11-05 23:09:25 +0900

    :seedling: feat: add CacheAspect bean to AppCtx and test multiple advices on a single pointcut in MainAspect

--- java/Spring/sp5-chap07/src/main/java/config/AppCtx.java
+++ java/Spring/sp5-chap07/src/main/java/config/AppCtx.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import aspect.CacheAspect;
 import aspect.ExeTimeAspect;
 import chap07.Calculator;
 import chap07.RecCalculator;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true) //자바 코드, "calculator" 프록시의 실제 타입은 RecCalculator를 상속받았으므로 RecCalculator로 타입 변환 가능
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 //@Enable ... : "..." 관련 기능을 적용하는데 필요한 다양한 스프링 설정 대신 처리 
 public class AppCtx {
 	@Bean
@@ -20,5 +21,10 @@ public class AppCtx {
 	@Bean
 	public Calculator calculator() {
 		return new RecCalculator();
+	}
+	
+	@Bean
+	public CacheAspect cacheAspect() {
+		return new CacheAspect();
 	}
 }
--- java/Spring/sp5-chap07/src/main/java/main/MainAspect.java
+++ java/Spring/sp5-chap07/src/main/java/main/MainAspect.java
@@ -10,10 +10,11 @@ public class MainAspect {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 		
 		Calculator cal = ctx.getBean("calculator", Calculator.class);
-//		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class); //타입불일치 : 프록시가 RecCalculator의 직접적인 구현체 및 자식이 아니기 때문에 예외(BeanNotOfRequiredTypeException) 발생
-		long fiveFact = cal.factorial(5);
-		System.out.println("cal.factorial(5) = " + fiveFact);
-		System.out.println(cal.getClass().getName());
+//		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+		cal.factorial(7);
+		cal.factorial(7);
+		cal.factorial(5);
+		cal.factorial(5);
 		ctx.close();
 	}
 }
