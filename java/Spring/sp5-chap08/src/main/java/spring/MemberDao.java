dcd12a7431f891d97360f2e86d3aaea5565dbbd9
ramgthunder <122340555+ramgthunder12@users.noreply.github.com>
2025-11-27 19:36:05 +0900

    :sparkles: feat: implement selectAll() in MemberDao

--- java/Spring/sp5-chap08/src/main/java/spring/MemberDao.java
+++ java/Spring/sp5-chap08/src/main/java/spring/MemberDao.java
@@ -1,11 +1,14 @@
 package spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import dbquery.MemberRowMapper;
 
@@ -63,8 +66,21 @@ public class MemberDao {
 		
 	}
 	
-	public Collection<Member> selectAll() {
-		return null;
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from MEMBER", new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(
+						rs.getString("EMAIL"),
+						rs.getString("PASSWORD"),
+						rs.getString("NAME"),
+						rs.getTimestamp("REGDATE").toLocalDateTime());
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		});
+		
+		return results;
 	}
 }
 
